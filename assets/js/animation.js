54a3afb5ebb92ffe20bb6c58211df3144a5b5228
Matthew <matthew12fy@gmail.com>
2021-04-10 15:57:10 -0500

    matthewfy

---
+++ assets/js/animation.js
@@ -0,0 +1,32 @@
+window.sr = new ScrollReveal();
+
+sr.reveal('.barra',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+sr.reveal('.inicio',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+sr.reveal('.abaut',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+sr.reveal('.portfolio',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+sr.reveal('.services',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+sr.reveal('.contact',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+// Comando de menu/navbar
+$('.barra-btn').click(function(){
+    $('.barra .menu').toggleClass("active");
+    $('.barra-btn i').toggleClass("active");
+});
